643b72af347ce7c0db19b05aaabdbec8976bdf48
junior <junior@istea.com.ar>
2022-07-10 02:38:27 -0300

    test

--- src/main/java/com/wu/billersplus/connector/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://operations.genericows.sibs.com.ar", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.wu.billersplus.connector.ws;
